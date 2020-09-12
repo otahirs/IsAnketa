6f3e351289a69df4a44f2da67b8a8fbada3482a5
Otakar Hirs <otakar.hirs@gmail.com>
2020-09-12 11:37:39 +0200

    logging to IS done

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace isanketa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = new CookieContainer();
+            
+            Utils.IsLogger(handler);
+
+            using (var client = new HttpClient(handler))
+            {
+                string baseUrl = "https://is.muni.cz/auth/";
+                var resSession = client.GetAsync(baseUrl).Result;
+                resSession.EnsureSuccessStatusCode();
+
+                var html = resSession.Content.ReadAsStringAsync().Result;
+                System.Console.WriteLine(html);
+
+                //var doc = new HtmlDocument();
+                //doc.LoadHtml(html);
+               
+            }
+        }
+    }
+}
